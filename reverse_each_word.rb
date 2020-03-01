c3c411b9cb52b4fb1caba4ccc7b80e1f44da0b79
Will Brennan <willfbren@gmail.com>
2020-03-01 02:01:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,5 +11,7 @@ def reverse_each_word(string)
   array = string.split(" ")
   new_array = []
   array.collect do |string|
-    new_array
+    new_array << string.reverse 
+  end 
+  new_array.join(" ")
 end
